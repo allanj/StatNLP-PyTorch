b256961a790a3a71c687e5c42fb88474cbca7912
Li Hao <lihao.leolee@gmail.com>
2018-02-28 11:27:33 +0800

    NetworkConfig

---
+++ NetworkConfig.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class NetworkConfig:
+    DEFAULT_CAPACITY_NETWORK = np.asarray([4096,4096,4096,4096,4096])
+
+
+
+if __name__ == "__main__":
+    print(NetworkConfig.DEFAULT_CAPACITY_NETWORK)
